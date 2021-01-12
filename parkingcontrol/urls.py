6fe3bae4217802c919c4ca52992c3232221f883e
Siddhartha Satyakama <bithal06@gmail.com>
2021-01-12 22:14:40 +0530

    deploying to heroku

--- parkingcontrol/urls.py
+++ parkingcontrol/urls.py
@@ -22,9 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    # path('', include('frontend.urls')),
-    # path('accounts/', include('accounts.urls')),
-    # path('apartment/', include('apartment.urls')),
-    # path('enforcement/', include('enforcementcompany.urls')),
-    # path('admin/', include('webadmin.urls')),
+    path('', include('frontend.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('apartment/', include('apartment.urls')),
+    path('enforcement/', include('enforcementcompany.urls')),
+    path('admin/', include('webadmin.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
